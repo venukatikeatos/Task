1d67bce8454e1bc692a99b2e72ffaed2af344d71
venukatikeatos <124596403+venukatikeatos@users.noreply.github.com>
2023-05-16 12:31:46 +0530

    Add files via upload

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public static FileResult CapturedPhoto { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            var photo = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+            {
+                Title = "Take a Photo"
+            });
+            CapturedPhoto = photo;
+        }
+
+
+
+        async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            var photo = await MediaPicker.CapturePhotoAsync();
+            CapturedPhoto = photo;
+        }
+    
+        async void Button_Clicked_2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+
+    }
+}
---
+++ App1/Page1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            var ph = MainPage.CapturedPhoto;
+            InitializeComponent();
+            if (ph != null)
+            {
+                MyImage.Source = ImageSource.FromFile(ph.FullPath);
+            }
+   
+        }
+        async void Button_Clicked_3(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2());
+        }
+    }
+}
